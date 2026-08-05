dad5703de2beb79b46846c5fa4212ca941668744
Shiroishiii <isaacdantas2503@gmail.com>
2026-08-04 21:51:24 -0300

    Finalizar Backend

---
+++ Implantação de sistemas/Aula 03-08/sistema de agendamento de limpeza/backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log(req)
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+    console.log("Servidor ta de pé :p")
+})
